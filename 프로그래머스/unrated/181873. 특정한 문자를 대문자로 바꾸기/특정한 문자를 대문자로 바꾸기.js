ff936142441ede4949aabc3306f801c68fdadc73
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-06 22:48:48 +0900

    Update 특정한 문자를 대문자로 바꾸기.js

--- 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -2,3 +2,7 @@ function solution(my_string, alp) {
     const result = my_string.split('').map(a => a === alp ? a.toUpperCase() : a).join("");
     return result;
 }
+
+// 다른 사람의 좋은 풀이
+// my_string 문자열 중 alp를 찾아 대문자로 만든 후 return
+const solution = (my_string, alp) => my_string.replaceAll(alp, alp.toUpperCase());
