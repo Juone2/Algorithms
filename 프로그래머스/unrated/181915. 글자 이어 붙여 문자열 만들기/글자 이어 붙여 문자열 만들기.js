9eb8882e02ae1fa6d206ea3d9410e21b21205083
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-07 20:11:21 +0900

    Update 글자 이어 붙여 문자열 만들기.js

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,3 +1,10 @@
 function solution(my_string, index_list) {
     return index_list.reduce((acc, idx) => acc + my_string[idx], "")
 }
+
+// 다른 사람의 좋은 풀이
+// map을 통해 배열을 순회 하며 각 인덱스르루 문자열로 반환함
+function solution(my_string, index_list) {
+    const result = index_list.map(i => my_string[i]).join('');
+    return result;
+}
