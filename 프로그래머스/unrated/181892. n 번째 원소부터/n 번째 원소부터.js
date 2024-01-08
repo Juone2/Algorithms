0d5f05b9b43775ceb2ba5e678675118ea319fee5
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-08 22:18:38 +0900

    Update n 번째 원소부터.js

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -2,3 +2,9 @@ function solution(num_list, n) {
     const result = num_list.slice(n - 1, num_list.length);
     return result;
 }
+
+// 다른 사람 좋은 풀이
+// 똑같은 slice인데 나만 어렵게 생각 했나봄
+function solution(num_list, n) {
+  return num_list.slice(n - 1);
+}
