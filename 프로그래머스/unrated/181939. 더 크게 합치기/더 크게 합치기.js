398d1bb3bc8c3a90a76bf25730556eac3f0a769c
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-10 09:42:26 +0900

    Update 더 크게 합치기.js

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,6 +1,13 @@
+// my solution
 function solution(a, b) {
     let result = Number(String(b) + String(a)) > Number(String(a) + String(b))
             ? Number(String(b) + String(a))
             : Number(String(a) + String(b))
     return result;
+}
+
+// the otehr solution
+// Math.max -> 입력 값 중 큰 값 반환
+function solution(a, b) {
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
 }
