9f88f6bd76daa0fcd586a646a54f812bc6d2d8cc
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-13 23:02:13 +0900

    Update 두 수의 연산값 비교하기.js

--- 프로그래머스/Lv.0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/Lv.0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -5,3 +5,12 @@ function solution(a, b) {
     let result = sumab > xab ? sumab : xab;
     return result;
 }
+
+// 다른 사람 풀이
+// a+""+b+"" -> 문자열 변환 
+function solution(a, b) {
+    let num1 = parseInt(a+""+b+"");
+    let num2 = 2*a*b;
+    return num1 > num2 ? num1 : num2;
+}
+
