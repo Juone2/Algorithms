94bd51c51b578675b747ed5b47ca9e4bbf99da37
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-19 20:58:58 +0900

    Update 할 일 목록.js

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,3 +1,6 @@
 function solution(todo_list, finished) {
     return todo_list.filter((_, idx) => finished[idx] !== true);
 }
+
+// 다른 사람 풀이
+return todo_list.filter((e,i) => !finished[i]); -> 앞에 !를 붙여서 boolean 반환..
