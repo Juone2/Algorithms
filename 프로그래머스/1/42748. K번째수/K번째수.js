69cef4f97ab832579d7282835d484866fca1a98f
주원 <103497968+Juone2@users.noreply.github.com>
2024-01-23 20:53:12 +0900

    Update K번째수.js

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -23,3 +23,16 @@ const solution = (array, commands) => {
     }
     return result;
 }
+
+// 메서드 풀이
+
+function solution(array, commands) {
+    let answer = [];
+    commands.forEach(command => {
+        const temp = array.slice(command[0] - 1, command[1]);
+        temp.sort((a,b) => a - b);
+        answer.push(temp[command[2] - 1]);
+    });  
+    return answer;
+
+}
