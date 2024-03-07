463efa5785ffd426f3d43b095800715f21d516a0
주원 <103497968+Juone2@users.noreply.github.com>
2024-03-07 16:04:02 +0900

    [level 0] Title: 접미사 배열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    const length = my_string.length;
+    return [...my_string].reduce((acc, cur, idx) => {
+        const result = my_string.slice(idx, length);
+        return [...acc, result]
+    }, []).sort();
+}
