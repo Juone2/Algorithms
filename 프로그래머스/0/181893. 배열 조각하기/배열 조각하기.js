5f1e50a3e17f8108a2c8ab6571cc4fea3a8452d4
주원 <103497968+Juone2@users.noreply.github.com>
2024-03-27 23:09:09 +0900

    UPDATE :: 다른 사람 풀이

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -5,3 +5,20 @@ function solution(arr, query) {
     }
     return arr;
 }
+
+ // 이 코드 좀 쩌는 듯
+function solution(arr, query) {
+    let [left, right] = [0, arr.length - 1];
+
+    query.forEach((item, idx) => {
+        (idx % 2) ? left += item : right = left + v;
+    })
+
+    const res = arr.slice(lt, rt);
+
+    if (!res.length) {
+        return [-1];
+    }
+
+    return res;
+}
