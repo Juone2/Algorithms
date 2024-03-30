1ab0723a50cef553b28b42a7e561cb15d36ea3a4
주원 <103497968+Juone2@users.noreply.github.com>
2024-03-30 23:08:21 +0900

    Update 간단한 식 계산하기.js

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -2,3 +2,18 @@
     const [a, op, b] = binomial.split(" ");
     return op === "+" ? +a + +b : op === "-" ? a - b : a * b
   };
+
+// 다른 풀이 1
+const ops = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+function solution(binomial) {
+  const [a, op, b] = binomial.split(' ');
+  return ops[op](+a, +b);
+}
+
+// 다른 사람 풀이 2
+const solution = (binomial) => eval(binomial); 
